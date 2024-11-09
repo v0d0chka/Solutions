7588c72ad3d42e06a67a4eb669712cc9f9719397
Vadym <vadik230311@gmail.com>
2024-11-09 17:51:45 +0200

    DZ

---
+++ DZ.py
@@ -0,0 +1,10 @@
+class Rectengle:
+    def __init__(self):
+     self.width = 148
+     self.height = 120
+    def calculate_area(self):
+     self.width + self.height
+    def calculate_perimiter(self):
+     self.widht self.height
+Reactangle1=()
+Reactangle1.printer()
